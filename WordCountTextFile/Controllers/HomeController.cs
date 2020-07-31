8b3452c76f44d082f3ec40f0d495e76a1c69489d
Diego Pitsillidis <diego.pitsillides@gmail.com>
2020-07-31 15:07:15 +0300

    finished, finialising needed

--- WordCountTextFile/Controllers/HomeController.cs
+++ WordCountTextFile/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WordCountTextFile.Models;
@@ -10,9 +12,46 @@ namespace WordCountTextFile.Controllers
 {
     public class HomeController : Controller
     {
+        static StreamReader URLStream(String fileurl)
+        {
+            return new StreamReader(new HttpClient().GetStreamAsync(fileurl).Result);
+        }
+
         public IActionResult Index()
         {
-            return View();
+            
+
+            string line;
+            string text = "";
+            //assuming no words are split between lines and lines have complete words
+            StreamReader s = URLStream(@"https://ia803105.us.archive.org/1/items/LordOfTheRingsApocalypticProphecies/Lord%20of%20the%20Rings%20Apocalyptic%20Prophecies_djvu.txt");
+            String myline = s.ReadLine(); //First Line
+            while ((line = s.ReadLine()) != null) //Subsequent Lines
+            {
+                line = line.ToLower();
+                text +=line;
+            }
+
+            var wordList = text.Split(" ");
+            var output = wordList.GroupBy(x => x).Select(x => new TextModel { Word = x.Key, Count = x.Count() }).OrderByDescending(x => x.Count);
+            string Result = "";
+            var i = 0;
+            foreach (var item in output)
+            {
+                if (i < 10)
+                {
+                    Result += item.Word + " : " + item.Count + Environment.NewLine;
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            //return View(Result);
+            return Content(Result);
         }
 
         public IActionResult Privacy()
