3ccddd420dc9db7358f56257c4ba8230a4758919
Diego Pitsillidis <diego.pitsillides@gmail.com>
2020-07-31 13:40:18 +0300

    initial

---
+++ WordCountTextFile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WordCountTextFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
